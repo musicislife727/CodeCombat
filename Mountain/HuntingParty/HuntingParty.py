f6632082344c9c6a0ac909e52032f095124f3552
musicislife727 <47801010+musicislife727@users.noreply.github.com>
2019-02-19 20:16:03 -0500

    Update HuntingParty.py
    Previous code did not work, and was a lot more than needed to be written.

--- Mountain/HuntingParty/HuntingParty.py
+++ Mountain/HuntingParty/HuntingParty.py
@@ -1,49 +1,25 @@
-# Прикажите вашим войскам двигаться на восток и атаковать любых огров которых они увидят.
-# Используйте for циклы и findFriends.
-# Вы можете использовать метод findNearestEnemy() на Ваших солдатах, для того, чтобы находить ближайшего к ним, а не к себе, противника.
-index = 0
+# You can use findNearestEnemy() on your soldiers to get their nearest enemy instead of yours
+
 while True:
+
     friends = hero.findFriends()
-    enemy = hero.findNearestEnemy()
-    if enemy and hero.distanceTo(enemy) < 20:
-        if (hero.isReady("jump") and hero.distanceTo(enemy) > 10):
-            hero.jumpTo(enemy.pos)
-        elif (hero.isReady("cleave")):
-            hero.cleave(enemy)
-        elif (hero.isReady("bash")):
-            hero.bash(enemy)
-        elif (hero.isReady("power-up")):
-            hero.powerUp()
-            hero.attack(enemy)
-        else:
-            hero.attack(enemy)
-    else:
-        if (hero.now() < 7):
-            pos = {'x': 87, 'y': 49}
-        else:
-            pos = {'x': 12, 'y': 47}
-        if (hero.isReady("jump")):
-            hero.jumpTo(pos)
-        else:
-            hero.move(pos)
-    for j in range(len(friends)):
-        friend = friends[j]
-        if (friend.pos.y > 46 and hero.now() < 5):
-            pos = {'x': 12, 'y': 64}
-        elif (friend.pos.y > 46):
-            if (friend.type != 'archer'):
-                pos = {'x': 61, 'y': 67}
-            else:
-                pos = {'x': 42, 'y': 67}
-        else:
-            if (friend.type != 'archer'):
-                pos = {'x': 45, 'y': 32}
-            else:
-                pos = {'x': 51, 'y': 32}
+
+    # Use for-loop and for each friend:
+    for friend in friends:
+
+        # If they see an enemy then command to attack.
+
         enemy = friend.findNearestEnemy()
-        if (friend.health < 29 or (enemy and friend.type == 'archer' and friend.distanceTo(enemy) < 22)):
-            hero.command(friend, 'move', {'x': 11, 'y': 47})
-        elif enemy and friend.distanceTo(enemy) < 20:
-            hero.command(friend, 'attack', enemy)
+
+        if enemy:
+
+            hero.command(friend, "attack", enemy)
+
+        # Command to move east by small steps.
+
         else:
-            hero.command(friend, 'move', pos)
+
+            moveTo = {"x": friend.pos.x + 0.35, "y": friend.pos.y}
+
+            hero.command(friend, "move", moveTo)
+
